5969aa6763301651eedfb210192617eeddbde197
your_github_Hadilou1998 <your_github_hadilissilame@gmail.com>
2024-07-01 13:01:32 +0200

    Updated index.js

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+import "./db.json";
 const express = require("express");
 const bodyParser = require("body-parser");
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.listen(port, () => {
+    console.log(`Server started at port http://localhost:${port}`);
+});
