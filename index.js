c6bb84092feb6f57498dadf927f34ea084454af5
your_github_Hadilou1998 <your_github_hadilissilame@gmail.com>
2024-07-01 14:49:04 +0200

    Updated index.js and Doctor.js

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 import "./db.json";
 const express = require("express");
 const bodyParser = require("body-parser");
+const fs = require("fs");
 
+// Create the express app
 const app = express();
 const port = 3000;
 
+// middleware
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
+// routes
+const route = require("./routes/Routes")
+app.use("/", routes)
 
-app.listen(port, () => {
-    console.log(`Server started at port http://localhost:${port}`);
+
+
+// util functions
+const saveData = (data) => {
+    const stringifyData = JSON.stringify(data)
+    fs.writeFileSync(data, stringifyData)
+}
+
+const getData = (data) => {
+    const jsonData = fs.readFileSync(data)
+    return JSON.parse(jsonData)
+}
+
+// start server
+app.listen(3000, () => {
+    console.log(`Server started at port http://localhost:3000`);
 });
